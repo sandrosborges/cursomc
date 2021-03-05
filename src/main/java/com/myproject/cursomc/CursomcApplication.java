0d966b2ada952cca818f230dea8a8919e3495846
Sandro S. Borges <>
2021-03-04 22:02:26 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/myproject/cursomc/CursomcApplication.java
+++ src/main/java/com/myproject/cursomc/CursomcApplication.java
@@ -92,6 +92,8 @@ public class CursomcApplication implements CommandLineRunner {
 		Endereco e1 = new Endereco(null, "Rua Flores","300","Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos","105","Sala 800", "Centro", "38777012", cli1, c2);
 		
+		
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
 		clienteRepo.saveAll(Arrays.asList(cli1));
--- src/main/java/com/myproject/cursomc/domain/Endereco.java
+++ src/main/java/com/myproject/cursomc/domain/Endereco.java
@@ -9,6 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Endereco  implements Serializable{
 
@@ -23,6 +26,7 @@ public class Endereco  implements Serializable{
 	private String bairro;
 	private String cep;
 
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
